ad550067c3e53fde2102e395fb0680c4b215ff81
Tricia Van Laar <tvanlaar@csustan.edu>
2024-03-04 15:50:34 -0800

    first commit

---
+++ carduelineCA/merge_sam_files_card.py
@@ -0,0 +1,18 @@
+# List of input file names
+input_files = ['pssam.txt', 'agsam.txt', 'lgsam.txt', 'hfsam.txt']
+
+# Initialize merged content with the first file's content (including header)
+merged_content = []
+with open(input_files[0], 'r') as first_file:
+    merged_content = first_file.readlines()
+
+# Read the content from the remaining files and append to merged content
+for input_file in input_files[1:]:
+    with open(input_file, 'r') as file:
+        content = file.readlines()
+        merged_content.extend(content[1:])  # Skip the header in subsequent files
+
+# Write the merged content to a new file
+with open('sampling.txt', 'w') as merged_file:
+    for line in merged_content:
+        merged_file.write(line)
